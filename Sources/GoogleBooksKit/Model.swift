5bb22ffc53509eaae0ba61686051976540fcc2dc
Ben Cardy <ben.cardy@ocado.com>
2024-05-23 15:14:28 +0100

    public public public

--- Sources/GoogleBooksKit/Model.swift
+++ Sources/GoogleBooksKit/Model.swift
@@ -43,114 +43,114 @@ struct GoogleBooksSearchQuery: StringKeyValueConvertible {
 
 // MARK: Responses
 
-struct GoogleBooksVolume: Codable, Identifiable {
-    let kind: String
-    let id: String
-    let etag: String
-    let selfLink: String
-    let volumeInfo: VolumeInfo
-    let saleInfo: SaleInfo?
-    let accessInfo: AccessInfo?
-    let searchInfo: SearchInfo?
+public struct GoogleBooksVolume: Codable, Identifiable {
+    public let kind: String
+    public let id: String
+    public let etag: String
+    public let selfLink: String
+    public let volumeInfo: VolumeInfo
+    public let saleInfo: SaleInfo?
+    public let accessInfo: AccessInfo?
+    public let searchInfo: SearchInfo?
     
-    struct VolumeInfo: Codable {
-        let title: String
-        let authors: [String]
-        let publisher: String?
-        let publishedDate: String?
-        let description: String?
-        let industryIdentifiers: [IndustryIdentifier]?
-        let readingModes: ReadingModes?
-        let pageCount: Int?
-        let printType: String?
-        let categories: [String]?
-        let maturityRating: String?
-        let allowAnonLogging: Bool?
-        let contentVersion: String?
-        let panelizationSummary: PanelizationSummary?
-        let imageLinks: ImageLinks?
-        let language: String?
-        let previewLink: String?
-        let infoLink: String?
-        let canonicalVolumeLink: String?
+    public struct VolumeInfo: Codable {
+        public let title: String
+        public let authors: [String]
+        public let publisher: String?
+        public let publishedDate: String?
+        public let description: String?
+        public let industryIdentifiers: [IndustryIdentifier]?
+        public let readingModes: ReadingModes?
+        public let pageCount: Int?
+        public let printType: String?
+        public let categories: [String]?
+        public let maturityRating: String?
+        public let allowAnonLogging: Bool?
+        public let contentVersion: String?
+        public let panelizationSummary: PanelizationSummary?
+        public let imageLinks: ImageLinks?
+        public let language: String?
+        public let previewLink: String?
+        public let infoLink: String?
+        public let canonicalVolumeLink: String?
     }
     
-    struct IndustryIdentifier: Codable {
-        let type: String
-        let identifier: String
+    public struct IndustryIdentifier: Codable {
+        public let type: String
+        public let identifier: String
     }
     
-    struct ReadingModes: Codable {
-        let text: Bool
-        let image: Bool
+    public struct ReadingModes: Codable {
+        public let text: Bool
+        public let image: Bool
     }
     
-    struct PanelizationSummary: Codable {
-        let containsEpubBubbles: Bool
-        let containsImageBubbles: Bool
+    public struct PanelizationSummary: Codable {
+        public let containsEpubBubbles: Bool
+        public let containsImageBubbles: Bool
     }
     
-    struct ImageLinks: Codable {
-        let smallThumbnail: String
-        let thumbnail: String
+    public struct ImageLinks: Codable {
+        public let smallThumbnail: String
+        public let thumbnail: String
     }
     
-    struct SaleInfo: Codable {
-        let country: String?
-        let saleability: String?
-        let isEbook: Bool?
-        let listPrice: Price?
-        let retailPrice: Price?
-        let buyLink: String?
-        let offers: [Offer]?
+    public struct SaleInfo: Codable {
+        public let country: String?
+        public let saleability: String?
+        public let isEbook: Bool?
+        public let listPrice: Price?
+        public let retailPrice: Price?
+        public let buyLink: String?
+        public let offers: [Offer]?
     }
     
-    struct Price: Codable {
-        let amount: Double?
-        let currencyCode: String?
+    public struct Price: Codable {
+        public let amount: Double?
+        public let currencyCode: String?
     }
     
-    struct Offer: Codable {
-        let finskyOfferType: Int?
-        let listPrice: MicrosPrice?
-        let retailPrice: MicrosPrice?
-        let giftable: Bool?
+    public struct Offer: Codable {
+        public let finskyOfferType: Int?
+        public let listPrice: MicrosPrice?
+        public let retailPrice: MicrosPrice?
+        public let giftable: Bool?
     }
     
-    struct MicrosPrice: Codable {
-        let amountInMicros: Int?
-        let currencyCode: String?
+    public struct MicrosPrice: Codable {
+        public let amountInMicros: Int?
+        public let currencyCode: String?
     }
     
-    struct AccessInfo: Codable {
-        let country: String?
-        let viewability: String?
-        let embeddable: Bool?
-        let publicDomain: Bool?
-        let textToSpeechPermission: String?
-        let epub: Epub?
-        let pdf: Pdf?
-        let webReaderLink: String?
-        let accessViewStatus: String?
-        let quoteSharingAllowed: Bool?
+    public struct AccessInfo: Codable {
+        public let country: String?
+        public let viewability: String?
+        public let embeddable: Bool?
+        public let publicDomain: Bool?
+        public let textToSpeechPermission: String?
+        public let epub: Epub?
+        public let pdf: Pdf?
+        public let webReaderLink: String?
+        public let accessViewStatus: String?
+        public let quoteSharingAllowed: Bool?
     }
     
-    struct Epub: Codable {
-        let isAvailable: Bool?
+    public struct Epub: Codable {
+        public let isAvailable: Bool?
     }
     
-    struct Pdf: Codable {
-        let isAvailable: Bool?
-        let acsTokenLink: String?
+    public struct Pdf: Codable {
+        public let isAvailable: Bool?
+        public let acsTokenLink: String?
     }
     
-    struct SearchInfo: Codable {
-        let textSnippet: String?
+    public struct SearchInfo: Codable {
+        public let textSnippet: String?
     }
 }
 
 public struct GoogleBooksVolumeResponse: Codable {
-    let kind: String
-    let totalItems: Int
-    let items: [GoogleBooksVolume]
+    public let kind: String
+    public let totalItems: Int
+    public let items: [GoogleBooksVolume]
 }
